bab5db6ddd9b18ffad4adb128124ef260a09f1cf
Nikolay Vaklev <58629389+nikolaychv@users.noreply.github.com>
2026-01-28 17:42:05 +0200

    Add ArraySum.java to calculate the sum of an array

---
+++ HackerRank/Algorithms/ArraySum.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int calculateSum(List<Integer> intArray) {
+        return intArray.stream().mapToInt(Integer::intValue).sum();
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        reader.readLine();
+
+        List<Integer> intArray = Stream.of(reader.readLine().trim().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        writer.write(Integer.toString(Result.calculateSum(intArray)));
+        writer.newLine();
+
+        reader.close();
+        writer.close();
+    }
+}
